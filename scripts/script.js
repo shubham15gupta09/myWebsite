f280192fba7f955f8b4ac89fb1b3429e84fc9124
Shubham Gupta <shubham15gupta09@gmail.com>
2020-11-28 22:38:58 +0530

    added the feature of view count

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,15 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function fetchView() {
+  var data = document.getElementById("views");
+  data.innerHTML = `<br/><div class="loader"></div>` ;
+  console.log("pressed");
+  fetch('https://viewcount.azurewebsites.net//checkcount')
+  .then(result => result.json())
+  .then(result => {
+    console.log(result);
+    data.innerHTML = "<br />Total View Count : " + result['view'];
+  })
+} 
